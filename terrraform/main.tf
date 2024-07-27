de9a2983d306029cf2a1c8de9ea5fb90b1fad49f
Adam Rocha <adamrocha13@gmail.com>
2024-07-27 16:58:02 +0000

    first commit

---
+++ terrraform/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      version = "~> 5.0.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "prometheus-430617"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.id
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
