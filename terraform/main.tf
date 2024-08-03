c4411201e5ed49e33dd8716c5ed26be2a8cf94e3
Adam Rocha <adamrocha13@gmail.com>
2024-08-03 00:36:41 +0000

    define static private ips for cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,8 +15,25 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+resource "google_compute_network" "vpc-network" {
+  name = "vpc-network"
+  auto_create_subnetworks = "true"
+}
+
+resource "google_compute_address" "static-a" {
+  name = "ipv4-address-a"
+}
+
+resource "google_compute_address" "static-b" {
+  name = "ipv4-address-b"
+}
+
+resource "google_compute_address" "static-c" {
+  name = "ipv4-address-c"
+}
+
+resource "google_compute_instance" "vm-instance-a" {
+  name         = "terraform-instance-a"
   machine_type = "e2-micro"
 
   boot_disk {
@@ -26,13 +43,45 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.id
+    network = google_compute_network.vpc-network.id
     access_config {
+      nat_ip = google_compute_address.static-a.address
     }
   }
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
+resource "google_compute_instance" "vm-instance-b" {
+  name         = "terraform-instance-b"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc-network.id
+    access_config {
+      nat_ip = google_compute_address.static-b.address
+    }
+  }
+}
+
+resource "google_compute_instance" "vm-instance-c" {
+  name         = "terraform-instance-c"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc-network.id
+    access_config {
+      nat_ip = google_compute_address.static-c.address
+    }
+  }
 }
