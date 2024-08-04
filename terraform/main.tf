a32a0eec1af50f6082f959e0d021f941fdc96ef3
Adam Rocha <adamrocha13@gmail.com>
2024-08-04 07:20:32 +0000

    isolate to one node

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,19 +20,33 @@ resource "google_compute_network" "vpc-network" {
   auto_create_subnetworks = "true"
 }
 
-resource "google_compute_address" "static-a" {
-  name = "ipv4-address-a"
+resource "google_compute_firewall" "egress" {
+  name      = "egress-firewall"
+  network   = google_compute_network.vpc-network.name
+  direction = "EGRESS"
+
+  allow {
+    protocol = "all"
+  }
 }
 
-resource "google_compute_address" "static-b" {
-  name = "ipv4-address-b"
+resource "google_compute_firewall" "ingress" {
+  name          = "ingress-firewall"
+  network       = google_compute_network.vpc-network.name
+  direction     = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443"]
+  }
 }
 
-resource "google_compute_address" "static-c" {
-  name = "ipv4-address-c"
+resource "google_compute_address" "ipv4-static-a" {
+  name = "ipv4-static-a"
 }
 
-resource "google_compute_instance" "vm-instance-a" {
+resource "google_compute_instance" "terraform-instance-a" {
   name         = "terraform-instance-a"
   machine_type = "e2-micro"
 
@@ -45,43 +59,7 @@ resource "google_compute_instance" "vm-instance-a" {
   network_interface {
     network = google_compute_network.vpc-network.id
     access_config {
-      nat_ip = google_compute_address.static-a.address
-    }
-  }
-}
-
-resource "google_compute_instance" "vm-instance-b" {
-  name         = "terraform-instance-b"
-  machine_type = "e2-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc-network.id
-    access_config {
-      nat_ip = google_compute_address.static-b.address
-    }
-  }
-}
-
-resource "google_compute_instance" "vm-instance-c" {
-  name         = "terraform-instance-c"
-  machine_type = "e2-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc-network.id
-    access_config {
-      nat_ip = google_compute_address.static-c.address
+      nat_ip = google_compute_address.ipv4-static-a.address
     }
   }
 }
